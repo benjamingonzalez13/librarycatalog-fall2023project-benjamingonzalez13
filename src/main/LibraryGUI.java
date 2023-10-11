3eab4dd447df155b89776f92c89594013e9de6a8
benjamingonzalez13 <124115105+benjamingonzalez13@users.noreply.github.com>
2023-10-11 16:10:21 -0400

    Update LibraryGUI.java
    Finished Library GUI

--- src/main/LibraryGUI.java
+++ src/main/LibraryGUI.java
@@ -4,121 +4,135 @@
  */
 package main;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.IOException;
-import javax.swing.*;
+import interfaces.List;
 
 public class LibraryGUI{
 	/**
-	 * This statement declares the variable libraryCatalog to be an instance of the class LibraryCatalog so we have access to its methods and variables.
+	 * This statement declares the variable libraryCatalog to be an instance of the class LibraryCatalog so we have access to its methods and variables. It also initiates
+	 * variables of frequent use in the class.
 	 */
-    static LibraryCatalog libraryCatalog;
+    static LibraryCatalog lc;
+    static JTextArea textArea;
+    static JScrollPane scrollPane;
     /**
      * This method is what makes the application run.
      * @param args command line arguments.
      */
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> GUI());
+        SwingUtilities.invokeLater(() -> GUI());  
     }
     /**
-     * This method initiates the variable libraryCatalog.
+     * 
+     * @param s String to be checked if is a valid number
+     * @return
      */
     public static void GUI(){
+    	/**
+    	 * This statements make sure to give title to the GUI and adjust sizes and backgrounds of the respective panel. It also makes sure to 
+    	 * close the GUI on clicking exit. 
+         */
+    	 JFrame frame = new JFrame();
+         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         JPanel panel = new JPanel();
+         panel.setPreferredSize(new Dimension(400, 400));
+         panel.setBackground(Color.BLUE);
+         /**
+          * This statement initiates the variable libraryCatalog. Calling lc would give us access to all
+          * LibraryCatalog public methods and variables.
+          */
         try{
-			libraryCatalog = new LibraryCatalog();
+			  lc = new LibraryCatalog();
 		}
         catch (IOException e){
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-        /**
-         * This statements make sure to give title to the GUI and adjust sizes and backgrounds of the respective panel.
-         */
-        JFrame frame = new JFrame("Library");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout());
-        panel.setPreferredSize(new Dimension(400, 400));
-        panel.setBackground(Color.BLACK);
+		}  
         /**
          * In this lines we create a method and a button, this method checks if a button was pressed. If the button was pressed we ask the user for the necessary
          * attributes a book has so we can add it. 
          */
         JButton addBookToCatalog = new JButton("Add Book to our  Catalog");  
-        addBookToCatalog.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                String title = JOptionPane.showInputDialog(frame, "What is the title for the book? ");
-                String author = JOptionPane.showInputDialog(frame, "What is the author for the book? ");
-                String genre = JOptionPane.showInputDialog(frame, "What is the genre for the book? ");
-                if(title != null && author != null && genre != null) {
-                    libraryCatalog.addBook(title, author, genre);
-                    JOptionPane.showMessageDialog(frame, "Just added the book " + title.toUpperCase() + " BY " + author.toUpperCase());
-                    JOptionPane.showMessageDialog(frame, "Thank You!");
-                }
+        addBookToCatalog.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                String inputTitle = JOptionPane.showInputDialog(frame, "What is the title for the book? ");
+                String inputAuthor = JOptionPane.showInputDialog(frame, "What is the author for the book? ");
+                String inputGenre = JOptionPane.showInputDialog(frame, "What is the genre for the book? ");
+                	if(inputTitle.length() != 0 && inputAuthor.length() != 0 && inputGenre.length() != 0){
+                		lc.addBook(inputTitle, inputAuthor, inputGenre);
+                		JOptionPane.showMessageDialog(frame, "Just added the book " + lc.getBookCatalog().last().toString());
+                		JOptionPane.showMessageDialog(frame, "Thank You!");
+                	}
+                	else {
+                		JOptionPane.showMessageDialog(frame, "Input was not valid");
+                	}
             }
         });
         panel.add(addBookToCatalog);
         /**
          * In this lines we create a method and a button, this method checks if a button was pressed. If the button was pressed go through the Users list displaying.
-         * Every User present in it. It does this by using a for each for loop. Adding them to a string and at the end displaying that string.  
+         * Every User present in it. It does this by using a for loop. Adding them to a string and at the end displaying that string.  
          */
         JButton displayCurrentUsers = new JButton("Display Users in system");
-        displayCurrentUsers.addActionListener(new ActionListener() {
-        	StringBuilder UsersInSystem = new StringBuilder();
+        displayCurrentUsers.addActionListener(new ActionListener(){
+        	String UsersInSystem = "";
+        	List<User> userList = lc.getUsers();
         	public void actionPerformed(ActionEvent e){
-        		for(User user : libraryCatalog.getUsers()){
-        			UsersInSystem.append(user.getName().toString()).append("\n");
+        		for(int i = 0; i < userList.size(); i ++){
+        			UsersInSystem += userList.get(i).getName() + "\n" ;
                 }
-                JTextArea textArea = new JTextArea(50, 50);
-                textArea.setEditable(false);
-                textArea.setText(UsersInSystem.toString());
-                JScrollPane scrollPane = new JScrollPane(textArea);
+                textArea = new JTextArea(50, 50);
+                textArea.setText(UsersInSystem);
+                scrollPane = new JScrollPane(textArea);
                 JOptionPane.showMessageDialog(frame, scrollPane, "Users in System", JOptionPane.PLAIN_MESSAGE);
         	}   	
         });
         panel.add(displayCurrentUsers);
         /**
          * In this lines we create a method and a button, this method checks if a button was pressed. If the button was pressed we remove the book that the User inputed the ID.
-         * We do this by making use of our remove function.  
+         * We do this by making use of our remove function. Then  display if the book was able to be removed based on the  size change. If a letter was inputed give an error message. 
          */
         JButton removeBookFromCatalog = new JButton("Remove Book from our Catalog");
         removeBookFromCatalog.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
-                String data = JOptionPane.showInputDialog(frame, "What is the Identifier of the book?");
-                if(data != null) {
-                    try{
-                        int bookId = Integer.parseInt(data);
-                        int size =  libraryCatalog.getBookCatalog().size();
-                        libraryCatalog.removeBook(bookId);
-                        if(size != libraryCatalog.getBookCatalog().size()) {
-                            JOptionPane.showMessageDialog(frame, "Book was able to be removed");
-                        }
-                        else{
-                            JOptionPane.showMessageDialog(frame, "Book doesn't exist");
-                        }
-                    } catch(NumberFormatException ex){
-                        JOptionPane.showMessageDialog(frame, "Book Identifier not valid, please verify format.");
-                    }
+                String data = JOptionPane.showInputDialog(frame, "What is the Identifier of the book?");                             	
+                if(data.length() != 0){
+                	try {  
+	                        int bookId = Integer.parseInt(data);
+	                        int prevSize = lc.getBookCatalog().size();
+	                        lc.removeBook(bookId);
+	                        if(prevSize == lc.getBookCatalog().size()){
+	                        	JOptionPane.showMessageDialog(frame, "Book doesn't exist");
+	                        }
+	                        else{
+	                            JOptionPane.showMessageDialog(frame, "Book was able to be removed");
+	                        }
+	                	}
+	                catch(NumberFormatException ex){
+	                        JOptionPane.showMessageDialog(frame, "Book Identifier not valid, please verify format.");
+	                	}
                 }
             }
-        });  
+        });
         panel.add(removeBookFromCatalog);
         /**
          * In this lines we create a method and a button, this method checks if a button was pressed. If the button was pressed go through the Catalog list displaying the 
-         * Every books present in it. It does this by using a for each for loop. Adding them to a string and at the end displaying that string.  
+         * Every books present in it. It does this by using a for loop. Adding them to a string and at the end displaying that string.  
          */
         JButton displayCatalog = new JButton("Display Catalog");
         displayCatalog.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
-                StringBuilder BookInCatalog = new StringBuilder();
-                for(Book book : libraryCatalog.getBookCatalog()){
-                	BookInCatalog.append(book.toString()).append("\n");
-                }
-                JTextArea textArea = new JTextArea(50, 50);
-                textArea.setEditable(false);
-                textArea.setText(BookInCatalog.toString());
-                JScrollPane scrollPane = new JScrollPane(textArea);
+            	String BookInSystem = "";
+            	List<Book> bookList = lc.getBookCatalog();
+            		for(int i = 0; i < bookList.size(); i ++){
+            			BookInSystem += bookList.get(i).toString() + "\n";
+                    }
+                textArea = new JTextArea(50, 50);
+                textArea.setText(BookInSystem);
+                scrollPane = new JScrollPane(textArea);
                 JOptionPane.showMessageDialog(frame, scrollPane, "Library Catalog", JOptionPane.PLAIN_MESSAGE);
             }
         });
